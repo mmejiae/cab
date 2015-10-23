dab464dcfec529c4c2a71ba0dc097cc5f5c0d6ae
Mariana <nanitamejes@hotmail.com>
2015-10-22 20:34:36 -0500

    clase 4

--- app/src/main/java/com/android/cursoandroid/MainActivity.java
+++ app/src/main/java/com/android/cursoandroid/MainActivity.java
@@ -3,54 +3,71 @@ package com.android.cursoandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView Estado;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    private String ciclo_vida;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Bind(R.id.toolbar)Toolbar toolbar;
+    Button iniciar;
+    Button finalizar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Estado= (TextView)findViewById(R.id.tv_estado);
-        ciclo_vida="Estado: onCreate";
-        Estado.setText(ciclo_vida);
+        ButterKnife.bind(this);
 
-        Log.d("CLASEANDROID", "ONCREATE");
+        toolbar.setTitle("Decoracion Navidad 2015");
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        setSupportActionBar(toolbar);
+
+        iniciar =(Button)findViewById(R.id.iniciar);
+        finalizar =(Button)findViewById(R.id.finalizar);
+
+        iniciar.setOnClickListener(this);
+        finalizar.setOnClickListener(this);
+/*
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              iniciarActivity(v);
+            }
+        });
+        finalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalizarActivity(v);
+            }
+        });
+        */
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("CLASEANDROID", "ONRESUME");
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("CLASEANDROID","ONSTART");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("CLASEANDROID", "ONPAUSE");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("CLASEANDROID", "ONSTOP");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("CLASEANDROID", "ONDESTROY");
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_msg:
+                Toast.makeText(MainActivity.this, "Feliz Navidad y prospero año nuevo", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void iniciarActivity(View view){
@@ -61,5 +78,16 @@ public class MainActivity extends AppCompatActivity {
     public void finalizarActivity(View view){
         finish();
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id= v.getId();
+        if(id==iniciar.getId()){
+            iniciarActivity(v);
+        }
+        else if(id==finalizar.getId()){
+            finalizarActivity(v);
+        }
     }
 }
